9f40d669aa03b69698321fb7c762435c21be93ca
denysbeckham <denys_jorge7@hotmail.com>
2024-05-20 19:45:39 -0400

    Ajuste em Caixa Eletronico

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,13 +1,22 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         
-        double saldo = 25.0;
+        double saldo = 300.0;
         double valorSolicitado = 217.0;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo) {
            saldo = saldo - valorSolicitado;
+           System.out.println("Saque Realizado, novo saldo: " + saldo);
+        }
+        else{
+        System.out.println("###### ATENÇÃO ######");
+        System.out.println("SAQUE NÃO REALIZADO, SALDO INSUFICIENTE...");
+        System.out.println("Saldo Atual: " + saldo);
+        System.out.println("Saque Solicitado: " + valorSolicitado);
+        
+        }
            
-        System.out.println(saldo);   
+           
 
     }
 }
