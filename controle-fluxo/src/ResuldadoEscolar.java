446ffce2133a4ace3fd3653b0ca9337e9c1236d8
denysbeckham <denys_jorge7@hotmail.com>
2024-05-21 10:04:29 -0400

    Ajuste na condição de nota

--- controle-fluxo/src/ResuldadoEscolar.java
+++ controle-fluxo/src/ResuldadoEscolar.java
@@ -1,9 +1,12 @@
 public class ResuldadoEscolar {
     public static void main(String[] args) {
-        int nota = 7;
+        int nota = 4;
 
         if(nota >= 7)
           System.out.println("Aprovado");
+
+          else if(nota >=5 && nota < 7)
+          System.out.println("Em Recuperação");
 
         else
           System.out.println("Reprovado");
